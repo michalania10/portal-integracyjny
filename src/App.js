5b88c437219736c5ceece524e6bfa596a513a836
Michal Wieczorek <michal.ania@gmail.com>
2023-02-07 13:23:20 +0100

    Render table for korba

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Table from './Table';
+import TranslationPl from './TranslationPl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table headers={["korba.leftCtx", "korba.result", "korba.rightCtx", "korba.text", "korba.date"]}
+           data={[
+             {
+               key: "someKey",
+               "korba.leftCtx": "Dobroć Pana w mnieyszych się ieszcze rzeczach wydaie iawnie;",
+               "korba.result": "pies [pies:subst:sg:nom:m]",
+               "korba.rightCtx": ", ktory wrot strzeże przez wzgląd bardziey na dawne",
+               "korba.text": "Monitor",
+               "korba.date": "1772"
+             }
+           ]}
+           translation={TranslationPl}/>
   );
 }
 
---
+++ src/TranslationPl.js
@@ -0,0 +1,11 @@
+import Table from "./Table";
+
+const TranslationPl = {
+  "korba.leftCtx": "Lewy kontekst",
+  "korba.result": "Rezultat",
+  "korba.rightCtx": "Prawy kontekst",
+  "korba.text": "Skrót tekstu",
+  "korba.date": "Data"
+};
+
+export default TranslationPl;
